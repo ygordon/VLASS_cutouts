7c40bc05a34a3083bab787301c77bcb23834f708
Yjan Gordon <yjangordon@Yjans-MBP.lan>
2022-02-10 11:37:03 -0600

    initial commit

---
+++ get_VLASS_cutouts.py
@@ -0,0 +1,262 @@
+###code to get VLASS cutout images from CADC
+
+import numpy as np, pyvo as vo, wget, argparse
+from astroquery.cadc import Cadc
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+from astropy.io import fits
+from reproject import reproject_interp
+from reproject.mosaicking import find_optimal_celestial_wcs, reproject_and_coadd
+from radio_beam import Beams
+
+
+#########################################################################
+#########################################################################
+###test parameters
+
+tp1 = SkyCoord('14h13m13.3s +46d06m53s')
+tp2 = SkyCoord(ra=224.43667, dec=54.743889, unit='deg')
+tp3 = SkyCoord('04h34m43.3s -39d00m07s')
+tsize = 2*u.arcmin
+
+#########################################################################
+#########################################################################
+
+def iau_name(ra, dec, aprec=2, dp=5, prefix=''):
+    ###truncated to dp so compatible with IAU standards
+    ra, dec = np.array(ra), np.array(dec)
+    
+    cat = SkyCoord(ra=ra, dec=dec, unit='deg')
+    
+    astring = cat.ra.to_string(sep='', unit='hour', precision=dp, pad=True)
+    dstring = cat.dec.to_string(sep='', precision=dp, pad=True, alwayssign=True)
+    
+    ###truncation index
+    tinda = aprec+7
+    tindd = tinda
+    if aprec==1:
+        tindd = tindd-1
+    if aprec<1:
+        tinda = tinda-1
+    
+    if prefix == '':
+        sname = ['J' + astring[i][:tinda] + dstring[i][:tindd] for i in range(len(astring))]
+    else:
+        sname = [prefix + ' J' + astring[i][:tinda] + dstring[i][:tindd] for i in
+                 range(len(astring))]
+    
+    return(sname)
+    
+    
+def get_coordlist(data, racol='RA', decol='DEC', posu='deg,deg'):
+    'extract a list of coordinates from data table'
+    if data[racol].unit is None or data[decol].unit is None:
+        coordlist = SkyCoord(ra=data[racol], dec=data[decol], unit=posu)
+    else:
+        coordlist = SkyCoord(ra=data[racol], dec=data[decol])
+        
+    return coordlist
+
+    
+def trim_slash(string):
+    'remove forward slash from end of string, e.g. allows functions here to work if given directory name of format dir/ rather than dir'
+    if string[-1] == '/':
+        string = string[:-1]
+    
+    return string
+    
+    
+def VLASS_image_query(position, size):
+    'query CADC for VLASS cutouts and return result'
+    radius = size/2
+    cadc = Cadc()
+    results = cadc.get_images(position, radius, collection='VLASS')
+    
+    ###create meta dict for image results
+    keys = []
+    for res in results:
+        head = res[0].header
+        epoch = '.'.join([head['FILNAM01'], head['FILNAM02']])
+        name = head['FILNAM05']
+        keys.append('_'.join([name, epoch]))
+        
+    outdict = {}
+    for i in range(len(keys)):
+        key = keys[i]
+        hdu = results[i]
+        outdict[key] = hdu
+    
+    return outdict
+    
+    
+def reduce_4dhdu_to_2d(hdu):
+    'ensure a 4d radio image hdu is in 2D for stacking'
+    ###only use with flat images not cubes!
+    ###extract 2D image array
+    imdata = hdu[0].data
+    if imdata.ndim == 4:
+        imdata = imdata[0][0]
+    
+        ###extract header info for 2D
+        head2d = hdu[0].header.copy()
+    
+        hkeys = list(head2d.keys())
+    
+        crkeys = ['CTYPE', 'CRVAL', 'CDELT', 'CRPIX', 'CUNIT']
+        cr3 = [c + '3' for c in crkeys]
+        cr4 = [c + '4' for c in crkeys]
+        badkeys = cr3 + cr4 + ['NAXIS3', 'NAXIS4']
+    
+        for key in hkeys:
+            if 'PC3' in key or 'PC4' in key or '_3' in key or '_4' in key:
+                badkeys.append(key)
+            if 'PC03' in key or 'PC04' in key or '_03' in key or '_04' in key:
+                badkeys.append(key)
+            if key in badkeys:
+                del(head2d[key])
+
+        head2d['NAXIS'] = 2
+
+    else:
+        head2d = hdu[0].header
+
+    ###create new 2D hdu
+    hdu2d = fits.PrimaryHDU(imdata)
+    hdu2d.header = head2d
+    hdulist2d = fits.HDUList(hdu2d)
+    
+    return(hdulist2d)
+    
+    
+def mosaic_2D_radio_images(hdus, common_beam=True):
+    'create optimised mosaic from multiple images including beam info'
+    ###get information from contributing headers
+    old_heads = [hdu[0].header for hdu in hdus]
+    
+    ###set up mosaic info
+    wcs_out, shape_out = find_optimal_celestial_wcs(hdus)
+    
+    ###stitch together data
+    array, footprint = reproject_and_coadd(hdus, wcs_out, shape_out=shape_out,
+                                           reproject_function=reproject_interp)
+                                           
+    new_hdu = fits.PrimaryHDU(array)
+    ###update WCS in header
+    new_hdu.header.update(wcs_out.to_header())
+    
+    ###beam info
+    image_beams = Beams([h['BMAJ'] for h in old_heads]*u.deg,
+                        [h['BMIN'] for h in old_heads]*u.deg,
+                        [h['BPA'] for h in old_heads]*u.deg)
+    
+    if common_beam == True:
+        mbeam = image_beams.common_beam() ##finds smallest common beam
+    else:
+        mbeam = image_beams.extrema_beams()[1] ###uses largest beam of set
+    
+    ###add to header
+    new_hdu.header.update(mbeam.to_header_keywords())
+    ###add in beam unit
+    new_hdu.header['BUNIT'] = old_heads[0]['BUNIT']
+    
+    ###filenames of contributing images
+    filenames =  ['.'.join([str(h[key]) for key in list(h.keys()) if 'FILNAM' in key]) for h in old_heads]
+    
+    ###create comment on mosaic
+    comment_1 = 'This cutout is a mosaic of the following contributing VLASS images obtained from CADC: '
+    comment_2 = ', '.join(filenames)
+    
+    new_hdu.header['COMMENT'] = ''.join([comment_1, comment_2])
+    
+    new_hdu = fits.HDUList(new_hdu)
+    
+    return new_hdu
+    
+    
+def obtain_preferred_cutout(results_dict, pref_epoch=1):
+    'output only the preferred epoch data'
+    
+    ###look for correct epoch in keys
+    epkey = 'VLASS' + str(pref_epoch)
+    keylist = list(results_dict.keys())
+    right_epoch = [key for key in keylist if epkey in key]
+    
+    ###sift out data
+    epoch_data = [reduce_4dhdu_to_2d(results_dict[key]) for key in keylist if key in right_epoch]
+    
+    ###if only one image returned then great, if more (e.g. e1.1, e1.2) stack/mosaic
+    if len(epoch_data) == 1:
+        hdu = epoch_data[0]
+    else:
+        hdu = mosaic_2D_radio_images(epoch_data)
+    
+    return hdu
+    
+    
+def download_cutouts(position, size, outdir='.', epoch=1, filename=None):
+    'take input position and cutout size to save single cutout to file'
+    ###can alter preferred epoch, will be a mosaic if multiple partial cutouts returned
+    outdir = trim_slash(outdir)
+    
+    ###obtain data
+    results = VLASS_image_query(position=position, size=size)
+    
+    ###extract required hdu
+    hdu = obtain_preferred_cutout(results_dict=results, pref_epoch=epoch)
+    
+    ###save to file
+    if filename is None:
+        source_name = iau_name(ra=[position.ra.value],
+                                   dec=[position.dec.value],
+                                   aprec=2)[0]
+        filename = '_'.join([source_name, '-'.join(['VLASS', str(epoch)])])
+        filename = '.'.join([filename, 'fits'])
+    
+    filename = '/'.join([outdir, filename])
+    
+    hdu.writeto(filename)
+    
+    return
+
+
+def parse_args():
+    "parse input args, i.e. target list, image, and rms file names"
+    parser = argparse.ArgumentParser(description="download cutout fits images from VLASS")
+    parser.add_argument("targets", help="file with list of sky coords to centre cutouts on")
+    parser.add_argument("--epoch", action="store", type=int,
+                        default=1, help="VLASS epoch to get images of")
+    parser.add_argument("--size", action="store", type=str,
+                        default="2arcmin", help="length of side of square cutout")
+    parser.add_argument("--racol", action="store", type=str,
+                        default="RA", help="column name with RA coordinates in target file")
+    parser.add_argument("--decol", action="store", type=str,
+                        default="DEC", help="column name with Dec. coordinates in target file")
+    parser.add_argument("--posunits", action="store", type=str,
+                        default="deg,deg", help="units to assume coordinates are in if not included in target table metadata")
+    parser.add_argument("--mosaic", action="store", type=str,
+                        default="True", help="mosaic if cutout straddles multiple images")
+    parser.add_argument("--outdir", action="store", type=str,
+                        default=".", help="directory to save files to")
+    args = parser.parse_args()
+    
+    ###convert arg.model_psf to bool
+    args.mosaic = bool(strtobool(args.mosaic))
+
+    return args
+
+#########################################################################
+#########################################################################
+###main
+
+
+if __name__ == "__main__":
+    ##parse command line arguments
+    args = parse_args()
+    
+    ##load data
+    targets = Table.read(args.targets)
+    coordinates = get_coordlist(data=targets, racol=args.racol,
+                                decol=args.decol, posu=args.posunits)
+    for co in coordinates:
+        download_cutouts(position=co, size=args.size, outdir=args.outdir,
+                         epoch=args.epoch)
